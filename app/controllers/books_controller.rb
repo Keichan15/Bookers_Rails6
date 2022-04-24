b836a333ac69104e0fb708ba591bc5a5a2e2cff1
Keichan15 <keisuke1515gaming@outlook.jp>
2022-04-24 09:19:21 +0000

    [Add] 編集画面と更新・削除機能、フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to books_path(@book.id)
     else
       @books = Book.all
@@ -25,8 +26,9 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    if book.update
-      redirect_to books_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
     else
       render :edit
     end
